8227f0768ae3e6d4b631ae8cee90f0f97090ab22
vinod-rajasekaran <flans-boules0n@icloud.com>
2025-05-13 15:56:48 +0530

    fixed start and end dates as string

--- models/intermediate/erp-common/int_projects.sql
+++ models/intermediate/erp-common/int_projects.sql
@@ -2,4 +2,11 @@
     tags=['projects']
 ) }}
 
-select * from {{ ref('stg_projects') }}
+select
+    name,
+    project_name,
+    status,
+    to_char(expected_start_date, 'DD-MM-YYYY') as expected_start_date,
+    to_char(expected_end_date, 'DD-MM-YYYY') as expected_end_date
+
+from {{ source('erp_next','tabProject') }}
