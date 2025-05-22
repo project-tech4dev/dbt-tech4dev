7870c4c4818e428ba7e7bb2dcdd53cf20bfe687a
mdshamoon <mohdshamoon841@gmail.com>
2025-05-22 23:07:57 +0530

    added project owner and docus areas

--- models/intermediate/erp-common/int_projects.sql
+++ models/intermediate/erp-common/int_projects.sql
@@ -1,13 +1,26 @@
 {{ config(
-    tags=['projects1']
+    tags=['projects']
 ) }}
 
 select
-    name,
-    project_name,
-    status,
-    owner,
-    to_char(expected_start_date, 'DD-MM-YYYY') as expected_start_date,
-    to_char(expected_end_date, 'DD-MM-YYYY') as expected_end_date
+    p.name,
+    p.project_name,
+    p.status,
+    e.employee_name as owner,
+    to_char(p.expected_start_date, 'DD-MM-YYYY') as expected_start_date,
+    to_char(p.expected_end_date, 'DD-MM-YYYY') as expected_end_date,
+    string_agg(f.focus_areas, ', ') as focus_areas
 
-from {{ source('erp_next','tabProject') }}
+from {{ source('erp_next','tabProject') }} p
+left join {{ source('erp_next','tabEmployee') }} e
+    on p.custom_project_owner = e.name
+left join {{ source('erp_next','tabFocus_Area_Table') }} f
+    on f.parent = p.name
+
+group by
+    p.name,
+    p.project_name,
+    p.status,
+    e.employee_name,
+    p.expected_start_date,
+    p.expected_end_date
