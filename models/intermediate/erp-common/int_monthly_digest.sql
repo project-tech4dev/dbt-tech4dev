1e02a4f23961b172446655202c3c597aa7e99c6a
mdshamoon <mohdshamoon841@gmail.com>
2025-05-26 14:05:44 +0530

    updated monthly digest

--- models/intermediate/erp-common/int_monthly_digest.sql
+++ models/intermediate/erp-common/int_monthly_digest.sql
@@ -27,7 +27,25 @@ SELECT
     plan_next_month,
     lessons_learned,
     last_edited_time,
-    creation_time
+    creation_time,
+    MAKE_DATE(
+        YEAR::INTEGER,
+        CASE MONTH
+            WHEN 'Jan' THEN 1
+            WHEN 'Feb' THEN 2
+            WHEN 'Mar' THEN 3
+            WHEN 'Apr' THEN 4
+            WHEN 'May' THEN 5
+            WHEN 'Jun' THEN 6
+            WHEN 'Jul' THEN 7
+            WHEN 'Aug' THEN 8
+            WHEN 'Sep' THEN 9
+            WHEN 'Oct' THEN 10
+            WHEN 'Nov' THEN 11
+            WHEN 'Dec' THEN 12
+        END,
+        1
+    ) AS report_date
 FROM
     source
 WHERE
--- models/production/erp-common/monthly_digest.sql
+++ models/production/erp-common/monthly_digest.sql
@@ -2,27 +2,7 @@
     tags = ['digests']
 ) }}
 
-WITH base AS (
-
-    SELECT
+SELECT
         *
     FROM
         {{ ref('int_monthly_digest') }}
-)
-SELECT
-    project,
-    project_name,
-    product_type,
-    YEAR,
-    MONTH,
-    hours_spend,
-    hours_committed,
-    highlights,
-    challenges,
-    unplanned_work,
-    plan_next_month,
-    lessons_learned,
-    last_edited_time,
-    creation_time
-FROM
-    base
