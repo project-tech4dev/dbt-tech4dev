8df4099983bbd7f496af79003b46284900a134cf
vinod-rajasekaran <flans-boules0n@icloud.com>
2025-07-18 22:18:00 +0530

    fixe date and text columns to fit normal expectations

--- models/intermediate/erp-common/int_projects.sql
+++ models/intermediate/erp-common/int_projects.sql
@@ -7,8 +7,10 @@ select
     p.project_name,
     p.status,
     e.employee_name as owner,
-    to_char(p.expected_start_date, 'DD-MM-YYYY') as expected_start_date,
-    to_char(p.expected_end_date, 'DD-MM-YYYY') as expected_end_date,
+    p.expected_start_date
+    p.expected_end_date,
+    to_char(p.expected_start_date, 'DD-MM-YYYY') as expected_start_date_text,
+    to_char(p.expected_end_date, 'DD-MM-YYYY') as expected_end_date_text,
     string_agg(f.focus_areas, ', ') as focus_areas
 
 from {{ source('erp_next','tabProject') }} p
