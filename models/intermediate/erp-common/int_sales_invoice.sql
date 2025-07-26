c0e44034a1eb34dce9f8b77ceb39306cb5fb9b6e
vinod-rajasekaran <flans-boules0n@icloud.com>
2025-07-26 23:06:09 +0530

    added sales invoices

--- models/intermediate/erp-common/int_sales_invoice.sql
+++ models/intermediate/erp-common/int_sales_invoice.sql
@@ -2,4 +2,4 @@
     tags = ['projects']
 ) }}
 
-select name,customer,status, due_date, outstanding_amount from {{ source('erp_next', 'sales_invoice') }}
+select product_type, name,customer,status, due_date, base_grand_total as outstanding_amount from {{ source('erp_next', 'sales_invoice') }}
