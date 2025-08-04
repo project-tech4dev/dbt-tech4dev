35332f44cecf700b3f652948cadc2465689daf81
vinod-rajasekaran <flans-boules0n@icloud.com>
2025-08-04 09:59:33 +0530

    added conditional for most recent report and also status of porject in monthly digest prod table

--- models/production/erp-common/monthly_digest.sql
+++ models/production/erp-common/monthly_digest.sql
@@ -3,6 +3,6 @@
 ) }}
 
 SELECT
-        *
+        *, CASE WHEN report_date = MAX(report_date) OVER (PARTITION BY project) THEN 'Yes' ELSE 'No' END AS is_most_recent
     FROM
         {{ ref('int_monthly_digest') }}
