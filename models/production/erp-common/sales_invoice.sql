9d274c2025381158fc35493a8dcec9a9acb21504
vinod-rajasekaran <flans-boules0n@icloud.com>
2025-10-21 13:46:38 +0530

    Update sales_invoice.sql

--- models/production/erp-common/sales_invoice.sql
+++ models/production/erp-common/sales_invoice.sql
@@ -2,4 +2,4 @@
     tags = ['projects']
 ) }}
 
-select *, datediff(now(),due_date) as invoice_ageing from {{ref('int_sales_invoice')}}
+select *, (now() - due_date)::int as invoice_ageing from {{ref('int_sales_invoice')}}
