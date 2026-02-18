3dbbd9b51eae7ddf76cd63cb949b71864284b668
Anusha <anushaganapathi@outlook.com>
2026-02-18 08:55:57 +0530

    format fixes - dalgo
    format fixes for data in the consulting time tracker

--- models/intermediate/dalgo/int_consulting_time_raw.sql
+++ models/intermediate/dalgo/int_consulting_time_raw.sql
@@ -3,7 +3,11 @@
 
 {{ config(materialized='table', schema='intermediate', tags=["consulting", "dalgo"]) }}
 WITH cte1 as (
-{{ dbt_utils.union_relations(relations=[source('staging_dalgo', 'consulting_time_tracker_anusha'),source('staging_dalgo', 'consulting_time_tracker_pratiksha'),source('staging_dalgo', 'consulting_time_tracker_ritabrata'),source('staging_dalgo', 'consulting_time_tracker_siddhant')] , include=['_airbyte_meta','end_date','logged_on_erp_','consultant','billing_status','attendees','description','num_charts_made','dashboard','project_program','start_date','category','_airbyte_raw_id','json_blob','hours_spent','ngo','_airbyte_extracted_at'] , source_column_name=None)}})
+{{ dbt_utils.union_relations(relations=[source('staging_dalgo', 'consulting_time_tracker_anusha'),
+source('staging_dalgo', 'consulting_time_tracker_pratiksha'),
+source('staging_dalgo', 'consulting_time_tracker_ritabrata'),
+source('staging_dalgo', 'consulting_time_tracker_siddhant')] ,
+ include=['_airbyte_meta','end_date','logged_on_ERP_','consultant','billing_status','attendees','description','num_charts_made','dashboard','project_program','start_date','category','_airbyte_raw_id','json_blob','hours_spent','ngo','_airbyte_extracted_at'] , source_column_name=None)}})
 -- Final SELECT statement combining the outputs of all CTEs
 SELECT *
 FROM cte1
