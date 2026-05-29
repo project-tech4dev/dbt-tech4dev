b89d1830ff4fb311c9980c10e300be62f95e173a
Ritabrata Roy <royred88@gmail.com>
2026-05-29 11:05:49 +0530

    bizdev- give.do NGO scrap results - prod model

---
+++ models/production/Bizdev/ngo_combined.sql
@@ -0,0 +1,31 @@
+{{ config(
+    materialized='table',
+    tags=['bizdev']
+) }}
+
+with source as (
+    select * from {{ source('bizdev_automation', 'Combined') }}
+),
+
+final as (
+    select
+        "FY_Year"              as fy_year,
+        "NGO_Name"             as ngo_name,
+        "HQ_Location"          as hq_location,
+        "Profile_URL"          as profile_url,
+        "Total_Revenue__FY___"::numeric as total_revenue,
+
+        case
+            when "Total_Revenue__FY___"::numeric > 200000000   then '>20Cr'
+            when "Total_Revenue__FY___"::numeric >= 100000000  then '10-20Cr'
+            when "Total_Revenue__FY___"::numeric >= 70000000   then '7-10Cr'
+            when "Total_Revenue__FY___"::numeric >= 40000000   then '4-70Cr'
+            when "Total_Revenue__FY___"::numeric >= 10000000   then '1-4Cr'
+            when "Total_Revenue__FY___"::numeric >= 0   then'<1Cr'
+            else null
+        end as revenue_category
+
+    from source
+)
+
+select * from final
